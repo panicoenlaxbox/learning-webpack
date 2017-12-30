f3c0fcc1027dd6ae089ab35ec3f9aa407f39c42c
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-12-30 13:08:58 +0100

    typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js' ]
+  },
   devServer: {
     contentBase: './dist'
   },
@@ -10,7 +23,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Sergio"
-    })
+    }),
+    new CleanWebpackPlugin(['dist'])
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
